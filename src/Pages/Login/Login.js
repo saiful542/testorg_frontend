eb8325841fd1a281f11143baf4a04d0edd4750c5
mahinhossen <mahinhossen001@gmail.com>
2022-11-08 18:41:53 +0600

    changed function await

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,184 +1,232 @@
 // import { data } from 'autoprefixer';
-import React, { useState } from 'react';
-import { useEffect } from 'react';
-import { useForm } from 'react-hook-form';
-import "./Login.scss"
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import "./Login.scss";
 const Login = () => {
-    const [isSignIn, setisSignIn] = useState(true);
-    const [signinData, setSigninData] = useState();
-    // const [signupData, setSignupData] = useState();
-    const { register, handleSubmit, reset } = useForm();
+  const [isSignIn, setisSignIn] = useState(true);
+  const [signinData, setSigninData] = useState();
+  // const [signupData, setSignupData] = useState();
+  const { register, handleSubmit, reset } = useForm();
 
-    // signup submit
-    const onSubmit = (submitted_data, e) => {
-        const loginEmailValue = document.querySelector('.login-email').value
-        const loginPasswordValue = document.querySelector('.login-password').value
-        const signupEmailValue = document.querySelector('.signup-email').value
-        const signupPasswordValue = document.querySelector('.signup-password').value
-        const signupUsernameValue = document.querySelector('.signup-username').value
-        let user;
+  // signup submit
+  const onSubmit = (submitted_data, e) => {
+    const loginEmailValue = document.querySelector(".login-email").value;
+    const loginPasswordValue = document.querySelector(".login-password").value;
+    const signupEmailValue = document.querySelector(".signup-email").value;
+    const signupPasswordValue =
+      document.querySelector(".signup-password").value;
+    const signupUsernameValue =
+      document.querySelector(".signup-username").value;
+    let user;
 
+    async function sendData(data) {
+      console.log("hello", data);
+      if (data) {
+        console.log("data found");
+        // fetch("https://testorg-backend.onrender.com/signup", {
+        const response = await fetch("http://localhost:4000/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+        const ret = await response.json();
+        console.log(ret);
+        //   .then((response) => response.json())
+        //   .then((data) => {
+        //     // console.log(data);
+        //   })
+        //   .catch((error) => {
+        //     // console.log(error);
+        //   });
+      } else console.log("data not found");
+    }
+    if (e.target.name == "signin") {
+      if (!loginEmailValue || !loginPasswordValue) {
+        alert("please, fill - up");
+      } else {
+        user = {
+          method: "signin",
+          loginEmail: submitted_data.loginEmail,
+          loginPassword: submitted_data.loginPassword,
+        };
+        sendData(user);
+        reset();
+      }
+    } else if (e.target.name == "signup") {
+      if (!signupEmailValue || !signupPasswordValue || !signupUsernameValue) {
+        alert("please, fill - up");
+      } else {
+        console.log("user created");
+        user = {
+          //   method: "signup",
+          email: submitted_data.email,
+          password: submitted_data.password,
+          usertype: submitted_data.profession,
+          username: submitted_data.username,
+        };
+        sendData(user);
+        reset();
+      }
+    }
+  };
 
-
-        const sendData = data => {
-            console.log('hello', data);
-            if (data) {
-                console.log('data found');
-                fetch('https://testorg-backend.onrender.com', {
-                    method: "POST",
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(data),
-                })
-                    .then(response => response.json())
-                    .then(data => {
-                        // console.log(data);
-                    })
-                    .catch((error) => {
-                        // console.log(error);
-                    })
-
-            }
-            else
-                console.log('data not found')
-
+  return (
+    <div className="">
+      <div
+        className={
+          isSignIn ? "containerr mb-0" : "containerr sign-up-mode mb-0"
         }
-        if (e.target.name == 'signin') {
-            if (!loginEmailValue || !loginPasswordValue) {
-                alert('please, fill - up')
-            }
-            else {
-                user = {
-                    method: 'signin',
-                    loginEmail: submitted_data.loginEmail,
-                    loginPassword: submitted_data.loginPassword
-
-                }
-                sendData(user)
-                reset();
-            }
-
-        }
-
-
-        else if (e.target.name == 'signup') {
-            if (!signupEmailValue || !signupPasswordValue || !signupUsernameValue) {
-                alert('please, fill - up')
-            }
-            else {
-                console.log('user created');
-                user = {
-                    method: 'signup',
-                    signupEmail: submitted_data.email,
-                    signupPassword: submitted_data.password,
-                    userType: submitted_data.profession,
-                    userName: submitted_data.username
-                }
-                sendData(user)
-                reset();
-            }
-        }
-    };
-
-
-    return (
-        <div className=''>
-            <div className={isSignIn ? 'containerr mb-0' : 'containerr sign-up-mode mb-0'}>
-                <div className="forms-container">
-                    <div className="signin-signup">
-                        <form onSubmit={handleSubmit(onSubmit)} name='signin' className="sign-in-form" id='sign-in-form'>
-                            <h2 className="title">Sign in</h2>
-                            <div className="input-field">
-                                <i className="fas fa-user"></i>
-                                <input type="email" placeholder="Email" {...register('loginEmail')} className="login-email" />
-                            </div>
-                            <div className="input-field">
-                                <i className="fas fa-lock"></i>
-                                <input type="password" placeholder="Password" {...register('loginPassword')} className='login-password' />
-                            </div>
-                            <input name='signin' type="submit" value="Login" className="btnn solid text-center" />
-                            <p className="social-text">Or Sign in with social platforms</p>
-                            <div className="social-media">
-                                <a href="#" className="social-icon">
-                                    <i className="fab fa-google"></i>
-                                </a>
-                                <a href="#" className="social-icon">
-                                    <i className="fab fa-linkedin-in"></i>
-                                </a>
-                            </div>
-                        </form>
-                        <form name='signup' onSubmit={handleSubmit(onSubmit)} className="sign-up-form" id='sign-up-form'>
-                            <h2 className="title">Sign up</h2>
-                            <div className="input-field d-flex text-center justify-content-between">
-                                <span className='w-100 d-flex justify-content-between text-center cs  '><i className="fas fa-chalkboard-user ci"></i>
-                                    <h6 className='  text-nowrap'>sign up as</h6></span>
-                                <span className='text-center'>
-                                    <select {...register("profession")} >
-                                        <option value="teacher">Teacher</option>
-                                        <option value="student">Student</option>
-                                    </select>
-                                </span>
-                            </div>
-                            <div className="input-field">
-                                <i className="fas fa-user"></i>
-                                <input type="text" placeholder="Username" {...register('username')} className="signup-username" />
-                            </div>
-                            <div className="input-field">
-                                <i className="fas fa-envelope"></i>
-                                <input type="email" placeholder="Email" {...register('email')} className="signup-email" />
-                            </div>
-                            <div className="input-field">
-                                <i className="fas fa-lock"></i>
-                                <input type="password" placeholder="Password" {...register('password')} className="signup-password" />
-                            </div>
-                            <input name='signup' type="submit" className="btnn" value="Sign up" />
-                            <p className="social-text">Or Sign up with social platforms</p>
-                            <div className="social-media">
-                                <a href="#" className="social-icon">
-                                    <i className="fab fa-google"></i>
-                                </a>
-                                <a href="#" className="social-icon">
-                                    <i className="fab fa-linkedin-in"></i>
-                                </a>
-                            </div>
-                        </form>
-
-                    </div>
-                </div>
-
-                <div className="panels-container">
-                    <div className="panel left-panel">
-                        <div className="content">
-                            <h3>New here ?</h3>
-                            <p>
-                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
-                                ex ratione. Aliquid!
-                            </p>
-                            <button onClick={() => setisSignIn(false)} className="btnn transparent" id="sign-up-btn">
-                                Sign up
-                            </button>
-                        </div>
-                        <img src="../files/login.svg" className="image" alt="" />
-                    </div>
-                    <div className="panel right-panel">
-                        <div className="content">
-                            <h3>One of us ?</h3>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-                                laboriosam ad deleniti.
-                            </p>
-                            <button onClick={() => setisSignIn(true)} className="btnn transparent" id="sign-in-btn">
-                                Sign in
-                            </button>
-                        </div>
-                        <img src="../files/signup.svg" className="image" alt="" />
-                    </div>
-                </div>
-            </div>
+      >
+        <div className="forms-container">
+          <div className="signin-signup">
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              name="signin"
+              className="sign-in-form"
+              id="sign-in-form"
+            >
+              <h2 className="title">Sign in</h2>
+              <div className="input-field">
+                <i className="fas fa-user"></i>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  {...register("loginEmail")}
+                  className="login-email"
+                />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-lock"></i>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  {...register("loginPassword")}
+                  className="login-password"
+                />
+              </div>
+              <input
+                name="signin"
+                type="submit"
+                value="Login"
+                className="btnn solid text-center"
+              />
+              <p className="social-text">Or Sign in with social platforms</p>
+              <div className="social-media">
+                <a href="#" className="social-icon">
+                  <i className="fab fa-google"></i>
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-linkedin-in"></i>
+                </a>
+              </div>
+            </form>
+            <form
+              name="signup"
+              onSubmit={handleSubmit(onSubmit)}
+              className="sign-up-form"
+              id="sign-up-form"
+            >
+              <h2 className="title">Sign up</h2>
+              <div className="input-field d-flex text-center justify-content-between">
+                <span className="w-100 d-flex justify-content-between text-center cs  ">
+                  <i className="fas fa-chalkboard-user ci"></i>
+                  <h6 className="  text-nowrap">sign up as</h6>
+                </span>
+                <span className="text-center">
+                  <select {...register("profession")}>
+                    <option value="teacher">Teacher</option>
+                    <option value="student">Student</option>
+                  </select>
+                </span>
+              </div>
+              <div className="input-field">
+                <i className="fas fa-user"></i>
+                <input
+                  type="text"
+                  placeholder="Username"
+                  {...register("username")}
+                  className="signup-username"
+                />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-envelope"></i>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  {...register("email")}
+                  className="signup-email"
+                />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-lock"></i>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  {...register("password")}
+                  className="signup-password"
+                />
+              </div>
+              <input
+                name="signup"
+                type="submit"
+                className="btnn"
+                value="Sign up"
+              />
+              <p className="social-text">Or Sign up with social platforms</p>
+              <div className="social-media">
+                <a href="#" className="social-icon">
+                  <i className="fab fa-google"></i>
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-linkedin-in"></i>
+                </a>
+              </div>
+            </form>
+          </div>
         </div>
-    );
+
+        <div className="panels-container">
+          <div className="panel left-panel">
+            <div className="content">
+              <h3>New here ?</h3>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Debitis, ex ratione. Aliquid!
+              </p>
+              <button
+                onClick={() => setisSignIn(false)}
+                className="btnn transparent"
+                id="sign-up-btn"
+              >
+                Sign up
+              </button>
+            </div>
+            <img src="../files/login.svg" className="image" alt="" />
+          </div>
+          <div className="panel right-panel">
+            <div className="content">
+              <h3>One of us ?</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+                laboriosam ad deleniti.
+              </p>
+              <button
+                onClick={() => setisSignIn(true)}
+                className="btnn transparent"
+                id="sign-in-btn"
+              >
+                Sign in
+              </button>
+            </div>
+            <img src="../files/signup.svg" className="image" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
-
